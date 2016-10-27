369d884015e41de887cb4ff74f5444f10d63e72d
Vanand Ter-Avetisyan <Vanand_Ter-Avetisyan@epam.com>
2016-10-27 16:37:10 +0400

    latest updates

--- main.cc
+++ main.cc
@@ -1,26 +1,23 @@
 #include <stdio.h>
 #include <ctime>
 
-
 unsigned long long f(unsigned long long n)
 {
-  if (n == 0)
-    return 1;
-  if(n == 1)
-    return 1;
-  if (n % 2 == 0)
-      return f(n / 2);
-    return f(n / 2) + f((n / 2) - 1);
+    if (n == 0)
+        return 1;
+    if (n == 1)
+        return 1;
+    if (n & 1) // n is odd 2k + 1
+        return f(n >> 1) + f((n >> 1) - 1);
+    return f(n >> 1); // n is even
 }
 
 int main()
 {
-      clock_t begin = clock();
-
-      f(123456789012345678);
-
-      clock_t end = clock();
-      double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-  printf("\ntime passed - %f sec\n", elapsed_secs);
-  return 0;
+    clock_t begin = clock(); // fixing time before executuion
+    printf("f(123456789012345678) = %lld\n", f(123456789012345678));
+    clock_t end = clock(); // fixing time after executuion
+    double elapsedTime = double(end - begin) / CLOCKS_PER_SEC;
+    printf("Calculation time - %f seconds\n", elapsedTime);
+    return 0;
 }
